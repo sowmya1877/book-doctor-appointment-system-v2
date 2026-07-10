7f90a318c5433e5e8628a7d4d114d0d7a3e15bb2
sowmya <sowmya824k@gmail.com>
2026-07-10 20:18:12 +0530

    Added Admin Login route

--- client-js/src/App.jsx
+++ client-js/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AdminLogin from "./pages/AdminLogin";
 import Doctors from "./pages/Doctors";
 import BookAppointment from "./pages/BookAppointment";
 import Admin from "./pages/Admin";
@@ -19,6 +20,7 @@ import Profile from "./pages/Profile";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <BrowserRouter>
@@ -29,35 +31,40 @@ function App() {
       {/* Main Content */}
       <div className="container mt-4 mb-5">
         <Routes>
+
           <Route path="/" element={<Home />} />
 
           <Route path="/login" element={<Login />} />
+
+          <Route path="/admin-login" element={<AdminLogin />} />
 
           <Route path="/register" element={<Register />} />
 
           <Route path="/doctors" element={<Doctors />} />
 
+          <Route path="/doctor/:id" element={<DoctorDetails />} />
+
           <Route path="/book" element={<BookAppointment />} />
 
           <Route path="/appointments" element={<Appointments />} />
+
           <Route path="/admin" element={<Admin />} />
+
           <Route path="/add-doctor" element={<AddDoctor />} />
+
           <Route path="/edit-doctor/:id" element={<EditDoctor />} />
+
+          <Route path="/profile" element={<Profile />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/about" element={<About />} />
+
           <Route
-  path="/appointments"
-  element={<Appointments />}
-/>
-<Route
-  path="/doctor/:id"
-  element={<DoctorDetails />}
-/>
-<Route path="/profile" element={<Profile />} />
-<Route path="/contact" element={<Contact />} />
-<Route path="/about" element={<About />} />
-<Route
-  path="/forgot-password"
-  element={<ForgotPassword />}
-/>
+            path="/forgot-password"
+            element={<ForgotPassword />}
+          />
+
         </Routes>
       </div>
 
